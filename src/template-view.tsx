a983b479a105983cf4f46282abd7d167bd193ea3
Mrid22 <mridulaga@outlook.com>
2025-08-12 22:00:29 +0200

    made array of dicts with mac address and name TODO: Map array to List.Items

--- src/template-view.tsx
+++ src/template-view.tsx
@@ -1,19 +1,35 @@
-import { ReactElement } from 'react';
-import { Detail } from "@project-gauntlet/api/components";
-import { Environment } from "@project-gauntlet/api/helpers";
+import { CommandContext } from "@project-gauntlet/api/helpers";
+import { ActionPanel, List } from "@project-gauntlet/api/components";
+import { ReactElement } from "react";
 
-export default function TemplateView(): ReactElement {
-    return (
-        <Detail>
-            <Detail.Content>
-                <Detail.Content.Paragraph>
-                    Hello Gauntlet!
-                </Detail.Content.Paragraph>
-                <Detail.Content.Paragraph>
-                    Gauntlet Version: {Environment.gauntletVersion}
-                </Detail.Content.Paragraph>
-            </Detail.Content>
-        </Detail>
-    );
-};
+export default function View(): ReactElement {
+  const command = new Deno.Command("bluetoothctl", {
+    args: ["devices", "Paired"],
+  });
 
+  const child = command.outputSync();
+
+  const stdout = new TextDecoder().decode(child.stdout);
+
+  console.dir(stdout);
+
+  const filteredLines = stdout
+    .split("\n")
+    .filter((device) => /Device/.test(device))
+    .map((device) => device.replace("Device ", ""));
+  const filteredDict: { mac: string; name: string }[] = filteredLines.map(
+    (device) => {
+      const result: string[] = device.split(" ");
+      const mac = result[0];
+      const name = result.slice(1).join(" ");
+      return { mac, name };
+    },
+  );
+  console.log(filteredDict);
+
+  return (
+    <List>
+      <List.SearchBar />
+    </List>
+  );
+}
